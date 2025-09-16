5698c371bb93e2e022eb6846f4596a9a27d80d8b
Francisco Gomez <franciscogomezcaceres@gmail.com>
2025-09-16 15:59:06 -0300

    agregado el registro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login
+from usuarios.forms import registrarse as FormularioRegistro
 
 def iniciar_sesion(request):
     
@@ -17,3 +18,19 @@ def iniciar_sesion(request):
         formulario = AuthenticationForm()
 
     return render(request, 'usuarios/iniciar_sesion.html', {'formulario': formulario})
+
+def registrarse(request):
+    
+    if request.method == 'POST':
+        formulario = FormularioRegistro(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            
+            return redirect('usuarios:iniciar_sesion')
+        
+    else:
+        formulario = FormularioRegistro()
+        
+    return render(request, 'usuarios/registrarse.html', {'formulario': formulario})
+            
+           
